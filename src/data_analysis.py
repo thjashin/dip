8cedf446ac4491e4046090419eb4a93a507c87e0
石佳欣 <ishijiaxin@126.com>
2014-06-09 00:09:08 +0800

    query search

--- src/data_analysis.py
+++ src/data_analysis.py
@@ -4,9 +4,21 @@
 # @file: data_analysis.py
 
 from __future__ import division
+import sys
+
+import ngram
+
 from config import *
 
 def query_existed():
+    """
+    Results:
+
+    train: 11701890
+    existed: 355
+    dev: 1000
+    existed ratio: 0.355
+    """
     train_queries = set()
     with open(train_click_log, 'r') as f:
         for line in f:
@@ -28,13 +40,34 @@ def query_existed():
     print 'existed ratio:', existed/total
 
 
-def query_fuzzy_existed():
+def query_search_test():
+    print 'generate queries in train set...'
+    sys.stdout.flush()
+
+    train_queries = []
     with open(train_click_log, 'r') as f:
         for line in f:
             arr = line.strip().split('\t')
             query = arr[1].strip()
             train_queries.append(query)
 
+    print 'generate queries in dev set...'
+    sys.stdout.flush()
+
+    dev_queries = []
+    with open(dev_label, 'r') as f:
+        for line in f:
+            arr = line.strip().split('\t')
+            query = arr[0].strip()
+            dev_queries.add(query)
+
+    print 'conduct a search...'
+    sys.stdout.flush()
+
+    G = ngram.NGram(train_queries)
+    print G.NGram.search(dev_queries[0], threshold=0.3)
+
 
 if __name__ == "__main__":
-    query_existed()
+    # query_existed()
+    query_search_test()
