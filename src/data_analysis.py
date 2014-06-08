e1b8de0cea37d62ce98e13271fff2e034ac98585
石佳欣 <ishijiaxin@126.com>
2014-06-08 23:47:36 +0800

    query

--- src/data_analysis.py
+++ src/data_analysis.py
@@ -22,9 +22,18 @@ def query_existed():
             dev_queries.add(query)
     existed = len(dev_queries.intersection(train_queries))
     total = len(dev_queries)
+    print 'train:', len(train_queries)
     print 'existed:', existed
     print 'dev:', total
     print 'existed ratio:', existed/total
+
+
+def query_fuzzy_existed():
+    with open(train_click_log, 'r') as f:
+        for line in f:
+            arr = line.strip().split('\t')
+            query = arr[1].strip()
+            train_queries.append(query)
 
 
 if __name__ == "__main__":
