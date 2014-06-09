7f80fe79b4e1ad6ea742bd0abc22c6f99bd6b5ca
石佳欣 <ishijiaxin@126.com>
2014-06-09 23:47:40 +0800

    m

--- src/retrieval.py
+++ src/retrieval.py
@@ -15,7 +15,7 @@ def create_index():
 
     with open(train_click_log, 'r') as f:
         last_image = None
-        query_doc = ''
+        query_doc = []
         for line in f:
             arr = line.strip().split('\t')
             image = arr[0].strip()
@@ -26,10 +26,13 @@ def create_index():
                 break
                 writer.add_document(img=last_image, query_doc=query_doc)
                 last_image = image
-                query_doc = ''
-            query_doc += ' ' + query
+                query_doc = []
+            try:
+                query_doc.append(unicode(query, 'utf-8'))
+            except UnicodeError:
+                pass
 
-        writer.add_document(img=image, query_doc=query_doc)
+        writer.add_document(img=image, query_doc=u' '.join(query_doc))
         writer.commit()
 
 
