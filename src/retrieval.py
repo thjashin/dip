8ba8d83eecea0a699b390832864d1ab9dec268aa
石佳欣 <ishijiaxin@126.com>
2014-06-29 16:02:10 +0800

    reduce bits

--- src/retrieval.py
+++ src/retrieval.py
@@ -160,8 +160,8 @@ def gist_top10_images(img):
     im = Image.open(img)
     im = crop_resize(im, normal_size, True)
     desc = leargist.color_gist(im)
-    for i in xrange(len(desc)):
-        desc[i] += 0.01
+    # for i in xrange(len(desc)):
+    #     desc[i] += 0.01
     res = lsh.query(desc, num_results=None, distance_func="hamming")
     print 'num of results:', len(res)
     for i in res:
