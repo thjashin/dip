4e86d24d51624e87924c9ab998bcd8f5e3069e39
石佳欣 <ishijiaxin@126.com>
2014-06-30 02:44:30 +0800

    test on Dev set

--- src/retrieval.py
+++ src/retrieval.py
@@ -152,19 +152,23 @@ def gist_top10_images(img):
     32 bit hash:
     bucket ratio: 225410/4294967296
     max conflicts: 8691
+
+    64 bit hash:
+    bucket ratio: 
+    max conficts: 
     """
     global lsh
 
-    # name2path = get_name2path(train_file_map)
+    name2path = get_name2path(train_file_map)
 
     # info of known dataset
-    # print lsh.hash_tables[0].keys()[0]
-    # print 'bucket ratio: %d/%d' % (len(lsh.hash_tables[0].keys()), 2 ** hash_len)
-    # counts = []
-    # t = lsh.hash_tables[0]
-    # for k in t.keys():
-    #     counts.append(len(t.get_list(k)))
-    # print 'max conflicts:', max(counts)
+    print lsh.hash_tables[0].keys()[0]
+    print 'bucket ratio: %d/%d' % (len(lsh.hash_tables[0].keys()), 2 ** hash_len)
+    counts = []
+    t = lsh.hash_tables[0]
+    for k in t.keys():
+        counts.append(len(t.get_list(k)))
+    print 'max conflicts:', max(counts)
 
     im = Image.open(img)
     im = crop_resize(im, normal_size, True)
@@ -172,10 +176,10 @@ def gist_top10_images(img):
     # for i in xrange(len(desc)):
     #     desc[i] += 0.01
     res = lsh.query(desc, num_results=10, distance_func="hamming")
-    # print 'num of results:', len(res)
-    # for i in res:
-    #     name = json.loads(i[0])[1]
-    #     print name, name2path[name], i[1]
+    print 'num of results:', len(res)
+    for i in res:
+        name = json.loads(i[0])[1]
+        print name, name2path[name], i[1]
 
     return [json.loads(i[0])[1] for i in res]
 
