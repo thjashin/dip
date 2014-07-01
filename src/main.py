1b174de1c77be2955bf090753c277793bf5bbabd
石佳欣 <ishijiaxin@126.com>
2014-07-01 16:19:57 +0800

    adjust hash length

--- src/main.py
+++ src/main.py
@@ -5,12 +5,13 @@
 
 from __future__ import division
 import sys
-import numpy as np
+import time
 
 from config import *
 from retrieval import query_top10_images, gist_top10_images, get_img2gist
 from utils import get_name2path
 
+import numpy as np
 from PIL import Image
 from cropresize import crop_resize
 import leargist
@@ -31,20 +32,29 @@ def main_proc(query, image_path):
     im = crop_resize(im, normal_size, True)
     desc = leargist.color_gist(im)
 
+    t1 = -time.time()
     A = query_top10_images(query)
+    print 'time for query_top10_images:', time.time() + t1
+
+    t2 = -time.time()
     B = gist_top10_images(image_path)
+    print 'time for gist_top10_images:', time.time() + t2
+
     if not A:
         query_miss += 1
         return np.random.rand()
     if not B:
         hash_miss += 1
     sim_set = []
+
+    t3 = -time.time()
     for i in A:
         gist_i = img2gist[i]
         for j in B:
             v = np.sum((gist_i - img2gist[j]) ** 2)
             sim_set.append(v)
         sim_set.append(np.sum((gist_i - desc) ** 2))
+    print 'time for compare:', time.time() + t3
 
     return min(sim_set)
 
